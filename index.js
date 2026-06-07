bf87efe26b657e8c9cfc0e62c3a2d496fc0abd0a
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-06-07 19:24:59 +0300

    17

--- index.js
+++ index.js
@@ -1434,3 +1434,188 @@ const b = a;
 //  })
 
 //==================================================================================
+
+//  function changeEven(numbers, value) {
+//    const newArray = [];
+//    for (let i = 0; i < numbers.length; i += 1) {
+//      if (numbers[i] % 2 === 0) {
+//        newArray.push(numbers[i] + value);
+//      } else {
+//        newArray.push(numbers[i]);
+//      }
+//    }
+//    return newArray;
+//  };
+//  
+//  changeEven([1, 2, 3, 4, 5], 10);
+//  console.log(changeEven([1, 2, 3, 4, 5], 10))
+//  console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); 
+//  changeEven([17, 24, 68, 31, 42], 100);
+//  console.log(changeEven([17, 24, 68, 31, 42], 100));
+//  console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+
+//==================================================================================
+
+//  const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+//  
+//  const planetsLengths = planets.map(function (planet) {
+//    return planet.length;
+//  });
+//  console.log(planetsLengths); // [5, 4, 6, 7]
+
+//==================================================================================
+
+//  const students = [
+//    { name: "Mango", score: 83 },
+//    { name: "Poly", score: 59 },
+//    { name: "Ajax", score: 37 },
+//    { name: "Kiwi", score: 94 },
+//    { name: "Houston", score: 64 },
+//  ];
+//  
+//  const names = students.map(student => student.name);
+//  console.log(names); // ["Mango", "Poly", "Ajax", "Kiwi", "Houston"]
+
+//==================================================================================
+//  
+//  const books = [
+//    {
+//      title: "The Last Kingdom",
+//      author: "Bernard Cornwell",
+//      rating: 8.38,
+//    },
+//    {
+//      title: "Beside Still Waters",
+//      author: "Robert Sheckley",
+//      rating: 8.51,
+//    },
+//    {
+//      title: "The Tell-Tale Heart",
+//      author: "Edgar Allan Poe",
+//      rating: 7.75,
+//    },
+//    { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//    { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+//  ];
+//  
+//  const titles = books.map(book => book.title);
+//  console.log(titles);
+
+// ==================================================================================
+
+//  const books = [
+//    {
+//      title: "The Last Kingdom",
+//      author: "Bernard Cornwell",
+//      genres: ["adventure", "history"],
+//    },
+//    {
+//      title: "Beside Still Waters",
+//      author: "Robert Sheckley",
+//      genres: ["fiction"],
+//    },
+//    {
+//      title: "Redder Than Blood",
+//      author: "Tanith Lee",
+//      genres: ["horror", "mysticism"],
+//    },
+//  ];
+//  
+//  const genres = books.flatMap(book => book.genres);
+//  console.log(genres); // ["adventure", "history", "fiction", "horror", "mysticism"]
+
+//==================================================================================
+
+//  const getUserEmails = (users) => users.map(user => user.email);
+//  const users = [
+//    {
+//      name: "Moore Hensley",
+//      email: "moorehensley@indexia.com",
+//      eyeColor: "blue",
+//      friends: ["Sharron Pace"],
+//      isActive: false,
+//      balance: 2811,
+//      skills: ["ipsum", "lorem"],
+//      gender: "male",
+//      age: 37,
+//    },
+//    {
+//      name: "Sharlene Bush",
+//      email: "sharlenebush@tubesys.com",
+//      eyeColor: "blue",
+//      friends: ["Briana Decker", "Sharron Pace"],
+//      isActive: true,
+//      balance: 3821,
+//      skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//      gender: "female",
+//      age: 34,
+//    },
+//    {
+//      name: "Ross Vazquez",
+//      email: "rossvazquez@xinware.com",
+//      eyeColor: "green",
+//      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//      isActive: false,
+//      balance: 3793,
+//      skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//      gender: "male",
+//      age: 24,
+//    },
+//    {
+//      name: "Elma Head",
+//      email: "elmahead@omatom.com",
+//      eyeColor: "green",
+//      friends: ["Goldie Gentry", "Aisha Tran"],
+//      isActive: true,
+//      balance: 2278,
+//      skills: ["adipisicing", "irure", "velit"],
+//      gender: "female",
+//      age: 21,
+//    },
+//    {
+//      name: "Carey Barr",
+//      email: "careybarr@nurali.com",
+//      eyeColor: "blue",
+//      friends: ["Jordan Sampson", "Eddie Strong"],
+//      isActive: true,
+//      balance: 3951,
+//      skills: ["ex", "culpa", "nostrud"],
+//      gender: "male",
+//      age: 27,
+//    },
+//    {
+//      name: "Blackburn Dotson",
+//      email: "blackburndotson@furnigeer.com",
+//      eyeColor: "brown",
+//      friends: ["Jacklyn Lucas", "Linda Chapman"],
+//      isActive: false,
+//      balance: 1498,
+//      skills: ["non", "amet", "ipsum"],
+//      gender: "male",
+//      age: 38,
+//    },
+//    {
+//      name: "Sheree Anthony",
+//      email: "shereeanthony@kog.com",
+//      eyeColor: "brown",
+//      friends: ["Goldie Gentry", "Briana Decker"],
+//      isActive: true,
+//      balance: 2764,
+//      skills: ["lorem", "veniam", "culpa"],
+//      gender: "female",
+//      age: 39,
+//    },
+//  ];
+//  console.log(getUserEmails(users));
+
+//==================================================================================
+
+//  const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+//  
+//  const evenNumbers = numbers.filter(number => number % 2 ===0);
+//  const oddNumbers = numbers.filter(number => number % 2 !==0);
+//  
+//  console.log(evenNumbers); 
+//  console.log(oddNumbers);
+
+//==================================================================================
