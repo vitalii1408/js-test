3b4b1daaf66a01f3750174060254b8462d283d3f
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-06-08 22:11:57 +0300

    17

--- index.js
+++ index.js
@@ -1813,4 +1813,32 @@ const b = a;
 
 //===========================================================================================
 
+//  const books = [
+//    {
+//      title: "The Last Kingdom",
+//      author: "Bernard Cornwell",
+//      rating: 8.38,
+//    },
+//    {
+//      title: "Beside Still Waters",
+//      author: "Robert Sheckley",
+//      rating: 8.51,
+//    },
+//    {
+//      title: "The Tell-Tale Heart",
+//      author: "Edgar Allan Poe",
+//      rating: 7.75,
+//    },
+//    { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//  ];
+//  const BOOK_TITLE = "The Tell-Tale Heart";
+//  const AUTHOR = "Robert Sheckley";
+//  
+//  const bookWithTitle = books;
+//  const bookByAuthor = books;
+
+//================================================================
+
+
+
 
